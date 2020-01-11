7e352ea7a0261bfd0f0dcd3459a97a73bc375c8c
xiaobo-hj <xiaobohj@163.com>
2020-01-11 13:58:35 +0800

    账单页面列表数据查询，以及store添加set方法

--- src/pages/bill/Bill.js
+++ src/pages/bill/Bill.js
@@ -1,18 +1,48 @@
 import React, {Component} from 'react';
 import {observer, inject} from "mobx-react";
 import {toJS} from 'mobx';
-import {PickerView} from 'antd-mobile';
-import {DatePicker, List} from 'antd-mobile';
+import {Picker, List} from 'antd-mobile';
 import "./Bill.less";
 
 @inject("billStore")
 @observer
 class Bill extends Component {
+  constructor(props) {
+    super(props);
+    const currentYear = new Date().getFullYear();
+    const yearData = []
+    for(var i=0;i<5;i++){
+      const label = currentYear-i+"年"
+      const value = currentYear-i
+      yearData.push({value:value+'',label:label})
+    }
+    this.state = {
+      yearData: yearData,
+      cols: 1,            //列数
+      asyncValue: [currentYear+''],
+      visible: false
+    };
+  }
+  onPickerChange(){
+
+  }
+  onClick(){
+
+  }
+  onOk(v){
+    this.setState({asyncValue:[v+'']})
+    //const year = new Date().getFullYear();
+    const {
+      queryBillList
+    } = this.props.billStore
+    queryBillList({year: v+''})
+  }
+
   render() {
     const {
       recorderSummaryData
     } = this.props.billStore
-    console.log(toJS(recorderSummaryData))
+    console.log(toJS(recorderSummaryData),'tttt')
     const me = this
     return (
 
@@ -20,11 +50,20 @@ class Bill extends Component {
         <div className="billHeader">
           <div className="billHeader_top">
             <div className="billHeader_top_left">
-              <p>结余</p>
-              <p>-132.00</p>
+              <p className="billHeader_top_left_balance_text">结余</p>
+              <p className="billHeader_top_left_balance">-132.00</p>
             </div>
             <div className="billHeader_top_right">
+              <Picker
+                data={this.state.yearData}
+                cols={this.state.cols}
+                value={this.state.asyncValue}
+                onPickerChange={this.onPickerChange}
+                onOk={v => this.onOk(v)}
 
+              >
+                <List.Item arrow="horizontal" onClick={this.onClick}></List.Item>
+              </Picker>
             </div>
           </div>
           <div className="billHeader_bottom">
@@ -38,6 +77,27 @@ class Bill extends Component {
             </div>
           </div>
         </div>
+        <div className="billBottom">
+          <div className="formheader" style={{display:'flex'}}>
+            <div className="formheaderItem">月份</div>
+            <div className="formheaderItem">收入</div>
+            <div className="formheaderItem">支出</div>
+            <div className="formheaderItem">结余</div>
+          </div>
+          {
+            recorderSummaryData.length>0 ?
+              recorderSummaryData.map(recordItem=>{
+                return(
+                  <div style={{display:'flex'}} key={recordItem.month} className="recorderList">
+                    <div className="recorderItem">{recordItem.month}</div>
+                    <div className="recorderItem">{recordItem.income}</div>
+                    <div className="recorderItem">{recordItem.pay}</div>
+                    <div className="recorderItem">{recordItem.balance}</div>
+                  </div>
+                )
+              }):null
+          }
+        </div>
       </div>
     );
   }
@@ -47,7 +107,6 @@ class Bill extends Component {
     const {
       queryBillList
     } = this.props.billStore
-    console.log("年份", year)
     queryBillList({year: year})
   }
 }
